6989fb806277389ec7cd9128817803426d1e4953
Alfred Rayo <ray22933@uvg.edu.gt>
2023-02-19 17:59:24 -0600

    Now Lisp is the base class, and can read input files.

---
+++ src/main/java/org/project/Lisp.java
@@ -0,0 +1,30 @@
+package org.project;
+
+import java.io.File;
+
+/**Class for starting execution*/
+public class Lisp
+{
+    /** Starts execution by getting the file where operations are*/
+    public static void main(String[] args) {
+        // IF NO FILE PATH GIVEN.
+        if (args.length == 0){
+            System.out.println("No file path for calculation given");
+            System.out.println("\tTry : java -jar <jarName> <filePath>");
+            System.out.println("\tExample : java -jar Calculator.jar ./operations.txt");
+        }
+        else{
+            File operationsFile = new File(args[0]);
+            // IF FILE NOT EXIST
+            if (!operationsFile.exists()){
+                System.out.println("Not such file exists.");
+                System.out.println("\tTry again, with a valid path.");
+            }
+            // IF FILE EXIST, START PROGRAM!
+            else{
+                Controller controller = Controller.getInstance(operationsFile);
+                controller.start();
+            }
+        }
+    }
+}
