dd1a846bcb93f9931f1dcdb90f158f60bbb7ccf7
Alfred Rayo <ray22933@uvg.edu.gt>
2023-02-28 22:29:27 -0600

    Fix: Update Regex Type patters to tolarate new data types.
    New data types : boolean operands, decimals

--- src/main/java/org/project/lexing/Patterns.java
+++ src/main/java/org/project/lexing/Patterns.java
@@ -6,11 +6,12 @@ package org.project.lexing;
  * */
 public class Patterns {
     public static final String WHITESPACE = "\\s";
-    public static final String NUMBER = "[0-9]+";
+    public static final String NUMBER = "-\\d+\\.?\\d*|\\d+\\.?\\d*";   // Accepts integers (1, -2) or decimal (0.0, 0.1)
     public static final String BOOLEAN = "T|NIL";
     public static final String LITERAL = "[a-zA-Z0-9]+";
     public static final String VALID_FUNCTION_NAME = "[a-zA-Z][a-zA-Z0-9]*";
     public static final String ARITHMETIC_OPERATOR = "[+\\-*/]";
+    public static final String LOGIC_OPERATOR = "=|<|>|<=|>=";
     public static final String EXPRESSION_OPENER = "\\(";
     public static final String EXPRESSION_CLOSER = "\\)";
 }
