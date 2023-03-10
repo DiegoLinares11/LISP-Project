9a19fa6f6f63c9c8acd627488a23790c0571d015
Alfred Rayo <ray22933@uvg.edu.gt>
2023-03-09 18:34:50 -0600

    Feat: Add javadoc for Lexer.java and TreeNode.java

--- src/main/java/org/project/lexing/Lexer.java
+++ src/main/java/org/project/lexing/Lexer.java
@@ -7,8 +7,19 @@ import java.util.List;
 
 import static org.project.helpers.StreamReader.streamToString;
 
+/** Representation of a LEXICAL ANALYZER... which means: it reads an input and gets its TOKENS
+ * (The Smallest lexical particles which have a meaning on LISP). Conforming the FIRST LAYER of code analysis.
+ * Ex: "- (+ 3 2) a" -> [ "-", "(", "+", "3", "2", ")" ,"a", ]
+ */
 public class Lexer {
 
+    /**
+     * Read the content of a stream, chop it, and return a list of the tokens it contains.
+     * Ex input : (+ 3 2)
+     * Ex output : ["+", "3", "2"]
+     * @param stream Stream input to read from.
+     * @return A list of the tokens contained in the input.
+     */
     public List<String> getTokens(InputStream stream){
         try {
             String expression = streamToString(stream);     // Read Stream.
@@ -20,11 +31,25 @@ public class Lexer {
         }
     }
 
+    /**
+     * Chops a string, and return a list of the tokens it contains.
+     * Ex input : "(+ 3 2)"
+     * Ex output : ["+", "3", "2"]
+     * @param  expression String input to read from.
+     * @return A list of the tokens contained in the input.
+     */
     public List<String> getTokens(String expression){
-        String depurateTokens = wrap(prepare(expression));
-        return divide(depurateTokens);
+        String depurateTokens = wrap(prepare(expression));  // Remove extra whitespaces and newlines, and wrap it.
+        return divide(depurateTokens);                      // Divide the tokens by whitespaces.
     }
 
+    /**
+     * Format a string for get its tokens, by removing extra whitespaces and newline characters..
+     * Ex input: "(+ 32 \n 2)"
+     * Ex output: " ( + 32 2 ) "
+     * @param expression Expression to prepare for future division.
+     * @return A String ready to be chopped in tokens..
+     */
     private String prepare (String expression){
         return expression.trim()
                 .replace(")", " ) ")
@@ -33,10 +58,26 @@ public class Lexer {
                 .replaceAll("\\s+", " ");
     }
 
+
+    /**
+     * Divides a string, and return a list of its tokens.
+     * Ex input : " ( + 3 2 ) "
+     * Ex output : ["+", "3", "2"]
+     * @param expression Expression to chop.
+     * @return A list of the tokens.
+     */
     private List<String> divide (String expression){
         return Arrays.asList(expression.split(" "));
     }
 
+    /**
+     * Adds "(" ")" at the start and end of and expression.
+     * Used to make sure everything is wrapped within an expression
+     * Ex input : " + 3 2 "
+     * Ex output : " ( + 3 2 ) "
+     * @param expression
+     * @return
+     */
     private String wrap (String expression){
         return "(" + expression + ")";
     }
--- src/main/java/org/project/parsing/TreeNode.java
+++ src/main/java/org/project/parsing/TreeNode.java
@@ -3,11 +3,20 @@ package org.project.parsing;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Representation of a Node, used for representing lisp data. A node can have child nodes, thus
+ * forming a tree, of data that could be traversed and evaluated.
+ */
 abstract class TreeNode {
 
+    /** List of all Nodes this Node is parent of.*/
     protected List<TreeNode> childNodes = new ArrayList<>();
 
-    // Recibes a valid expression (no missing parenthesis)
+    /**
+     * Transforms a list of tokens in a Node.
+     * @param tokens A valid list of tokens (Following Lisp sintaxis).
+     * @return A Node that represents the given data.
+     */
     public TreeNode getInstance(ArrayList<String> tokens){
         if (tokens.size() == 1)
             return new Atom(tokens.get(0));
@@ -17,6 +26,11 @@ abstract class TreeNode {
             throw new RuntimeException("ERROR: Attempted to create a node without data.");
     }
 
+    /**
+     * Transforms a string input in a Node.
+     * @param value A valid list of tokens (Following Lisp sintaxis).
+     * @return A Node that represents the given data.
+     */
     public TreeNode getInstance(String value){
         if (value.length() > 1)
             return new SExpression(value);
@@ -24,20 +38,43 @@ abstract class TreeNode {
             return new Atom(value);
     }
 
+    /**
+     * Returns a Node containing a given bool value.
+     * @param boolValue Bool value.
+     * @return A Node that represents the given data.
+     */
     public TreeNode getInstance(boolean boolValue){
         return new Atom(boolValue);
     }
 
+    /**
+     * Returns a Node containing a given numeric value.
+     * @param numeric numeric value.
+     * @return A Node that represents the given data.
+     */
     public TreeNode getInstance(int numeric){
         return new Atom(numeric);
     }
 
+    /**
+     * @return The list of child nodes this node is parent of.
+     */
     public List<TreeNode> getChildNodes() {
         return childNodes;
     }
 
+    /**
+     * @return True : is a Expression, False: is an atom.
+     */
     abstract public boolean isSExpression();
 
+    /**
+     * Evaluate a given node recursively, and return a new node with the value.
+     * Ex inputNode : (+ (+ 3 2) 3)
+     * Ex outputNode : (8)
+     * @param context Context of user defined variables and functions.
+     * @return Evaluated node.
+     */
     abstract public TreeNode evaluate(Context context);
 
     @Override
