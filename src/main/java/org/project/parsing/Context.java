5c4174b508c3d395f09bec4847fa19c082314143
Alfred Rayo <ray22933@uvg.edu.gt>
2023-03-12 17:28:23 -0600

    Upgrade: Enable context recursivity: Context can contains other Context.

--- src/main/java/org/project/parsing/Context.java
+++ src/main/java/org/project/parsing/Context.java
@@ -2,37 +2,56 @@ package org.project.parsing;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 public class Context {
     private final Map<String, TreeNode> variables = new HashMap<>();
-    private final Map<String, TreeNode> customFunctions = new HashMap<>();
+    private final Map<String, Function> customFunctions = new HashMap<>();
+    private Context parentContext;
+
+    public Context (){}
+
+    public Context (Context parentContext){
+        this.parentContext = parentContext;
+    }
 
     public boolean variableExist (String variableName){
         if (this.variables.isEmpty())
             return false;
-        return variables.containsKey(variableName);
+        if (this.variables.containsKey(variableName))
+            return true;
+        if (this.parentContext != null)
+            return parentContext.variableExist(variableName);
+        return false;
     }
 
     public boolean functionExist (String variableName){
         if (this.customFunctions.isEmpty())
             return false;
-        return customFunctions.containsKey(variableName);
+        if (this.customFunctions.containsKey(variableName))
+            return true;
+        if (this.parentContext != null)
+            return parentContext.functionExist(variableName);
+        return false;
     }
 
     public TreeNode getVariable (String variableName){
             TreeNode value = this.variables.get(variableName);
-            if (value.equals(null))
-                throw new RuntimeException("Error: Variable " + variableName + "is not declared");
-            else
-                return value;
+            if (value == null && this.parentContext != null)  // If not found, try in parent context.
+                value = this.parentContext.getVariable(variableName);
+            if (value == null)
+                throw new RuntimeException("ERROR: Variable " + variableName + "is not declared");
+            return value;
     }
 
-    public TreeNode getFunction (String functionName){
-        TreeNode value = this.variables.get(functionName);
-        if (value.equals(null))
-            throw new RuntimeException("Error: Function " + functionName + "is not declared");
-        else
-            return value;
+    public Function getFunction (String functionName){
+        Function value = this.customFunctions.get(functionName);
+        if (value == null && this.parentContext != null)  // If not found, try in parent context.
+            value = this.parentContext.getFunction(functionName);
+        if (value == null)
+            throw new RuntimeException("ERROR: Function " + functionName + "is not declared");
+        return value;
+
     }
 
     public void setVariable (String variableName, TreeNode value){
