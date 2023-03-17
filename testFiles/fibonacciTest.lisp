9ae88abb2189bbf8f6599cc46a2391b4c31f02c6
Alfred Rayo <ray22933@uvg.edu.gt>
2023-03-17 11:15:01 -0600

    Update: Classes UML

--- testFiles/fibonacciTest.lisp
+++ testFiles/fibonacciTest.lisp
@@ -1,8 +1,13 @@
+; Fibonacci function
 (defun fibonacci (n)
     (cond
-        ((< n 2) n)
+        ((< n 2) n) ; else try
         (T ( + (fibonacci (- n 1)) (fibonacci (- n 2))))
     )
 )
 
+; Calling function
+(print (fibonacci 10))
+(fibonacci 5)
+(fibonacci 5)
 (fibonacci 5)
